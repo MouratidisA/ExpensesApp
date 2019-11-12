e3f3d0cebd33977a3a68a6838bf1b8abdb8ceb6e
Mouratidis Anargyros <argiris95@windowslive.com>
2019-11-12 12:24:20 +0200

    [Droid] Custom Render for View Cells  (Selected item colors) Added

---
+++ ExpensesApp/ExpensesApp.Android/CustomRenderers/CustomViewCellRenderer.cs
@@ -0,0 +1,46 @@
+﻿using Android.Content;
+using Android.Graphics.Drawables;
+using Android.Views;
+using ExpensesApp.Droid.CustomRenderers;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+using View = Android.Views.View;
+
+[assembly: ExportRenderer(typeof(ViewCell), typeof(CustomViewCellRenderer))]
+namespace ExpensesApp.Droid.CustomRenderers
+{
+    class CustomViewCellRenderer : ViewCellRenderer
+    {
+        private Android.Views.View _cell;
+        private bool _isSelected;
+        private Drawable _defaultBackground;
+
+        protected override View GetCellCore(Cell item, View convertView, ViewGroup parent, Context context)
+        {
+            _cell= base.GetCellCore(item, convertView, parent, context);
+            _isSelected = false;
+            _defaultBackground = _cell.Background;
+            return _cell;
+        }
+
+        protected override void OnCellPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            base.OnCellPropertyChanged(sender, e);
+
+            if (e.PropertyName == "IsSelected")
+            {
+                _isSelected = !_isSelected;
+
+                if (_isSelected)
+                {
+                    _cell.SetBackgroundColor(Color.FromHex("#E6E6E6").ToAndroid());
+                }
+                else
+                {
+                    _cell.Background = _defaultBackground;
+                }
+            }
+        }
+    }
+}
