9a6f929a2ef3dde2d060c1208aa94c725fce7adc
Mouratidis Anargyros <argiris95@windowslive.com>
2019-11-12 13:59:47 +0200

    [Droid] Show functionality for share Dependency implemented
    - Read/Write External Permissions added in manifest
    - File Provider deceleration in manifest for application

--- ExpensesApp/ExpensesApp.Android/Dependencies/Share.cs
+++ ExpensesApp/ExpensesApp.Android/Dependencies/Share.cs
@@ -1,6 +1,8 @@
-﻿using System.Threading.Tasks;
+﻿using Android.Content;
+using Android.Support.V4.Content;
 using ExpensesApp.Droid.Dependencies;
 using ExpensesApp.Interfaces;
+using System.Threading.Tasks;
 using Xamarin.Forms;
 
 
@@ -9,16 +11,24 @@ namespace ExpensesApp.Droid.Dependencies
 {
     class Share:IShare
     {
-
-        public Share()
+ 
+        public Task Show(string title, string message, string filePath)
         {
+            var intent = new Intent(Intent.ActionSend);
+            intent.SetType("text/plain");
+            var documentUri = FileProvider.GetUriForFile(Forms.Context.ApplicationContext,
+                "om.companyname.ExpensesApp.provider", new Java.IO.File(filePath));
 
-        }
+            intent.PutExtra(Intent.ExtraStream, documentUri);
+            intent.PutExtra(Intent.ExtraText, title);
+            intent.PutExtra(Intent.ExtraSubject,message);
 
+            var chooserIntent = Intent.CreateChooser(intent, title);
+            chooserIntent.SetFlags(ActivityFlags.GrantReadUriPermission);
 
-        public async Task Show(string title, string message, string filePath)
-        {
-            throw new System.NotImplementedException();
+            Android.App.Application.Context.StartActivity(chooserIntent);
+
+            return Task.FromResult(true);
         }
     }
 }
