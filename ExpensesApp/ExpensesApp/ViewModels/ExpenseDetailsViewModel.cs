b5aafe3735ba2a58422fedf3a1ecf1fe07d5b450
MouratidisA <argiris95@windowslive.com>
2020-01-03 21:53:19 +0200

    ExpenseDetailsPage & ExpenseDetailsViewModel added

---
+++ ExpensesApp/ExpensesApp/ViewModels/ExpenseDetailsViewModel.cs
@@ -0,0 +1,29 @@
+﻿using ExpensesApp.Annotations;
+using ExpensesApp.models;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace ExpensesApp.ViewModels
+{
+    public class ExpenseDetailsViewModel :INotifyPropertyChanged
+    {
+        private Expense _expense;
+        public Expense Expense
+        {
+            get { return _expense; }
+            set
+            {
+                _expense = value;
+                OnPropertyChanged("Expense");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ ExpensesApp/ExpensesApp/Views/ExpenseDetailsPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using ExpensesApp.models;
+using ExpensesApp.ViewModels;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ExpensesApp.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ExpenseDetailsPage : ContentPage
+    {
+        public ExpenseDetailsViewModel ViewModel;
+
+        public ExpenseDetailsPage()
+        {
+            InitializeComponent();
+        }
+
+        public ExpenseDetailsPage(Expense expense)
+        {
+            InitializeComponent();
+            ViewModel.Expense = expense;
+        }
+    }
+}
