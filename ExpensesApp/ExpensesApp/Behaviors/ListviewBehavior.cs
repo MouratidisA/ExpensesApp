213107e79ce9305eebb9b0e7671950d7c66449e2
MouratidisA <argiris95@windowslive.com>
2020-01-03 21:55:29 +0200

    Adding Listview Behavior for Expense item selection

---
+++ ExpensesApp/ExpensesApp/Behaviors/ListviewBehavior.cs
@@ -0,0 +1,29 @@
+﻿using ExpensesApp.models;
+using ExpensesApp.Views;
+using Xamarin.Forms;
+
+namespace ExpensesApp.Behaviors
+{
+    class ListviewBehavior:Behavior<ListView>
+    {
+        private ListView _listView;
+        protected override void OnAttachedTo(ListView bindable)
+        {
+            base.OnAttachedTo(bindable);
+            _listView = bindable;
+            _listView.ItemSelected += ListView_ItemSelected;
+        }
+
+        protected override void OnDetachingFrom(ListView bindable)
+        {
+            base.OnDetachingFrom(bindable);
+            _listView.ItemSelected -= ListView_ItemSelected;
+        }
+
+        void ListView_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            Expense selectedExpense = _listView.SelectedItem as Expense;
+            App.Current.MainPage.Navigation.PushAsync(new ExpenseDetailsPage(selectedExpense));
+        }
+    }
+}
--- ExpensesApp/ExpensesApp/models/Expense.cs
+++ ExpensesApp/ExpensesApp/models/Expense.cs
@@ -5,7 +5,7 @@ using SQLite;
 
 namespace ExpensesApp.models
 {
-    class Expense
+    public class Expense
     {
         [PrimaryKey, AutoIncrement]
         public int Id { get; set; }
